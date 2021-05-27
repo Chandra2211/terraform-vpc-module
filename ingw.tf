0e1dca174322d019c49ee82db007ffb8bd6ed997
Chandra2211 <84915416+Chandra2211@users.noreply.github.com>
2021-05-27 21:06:03 +0530

    Add files via upload

--- ingw.tf
+++ ingw.tf
@@ -1,7 +1,7 @@
 # Creating Internet Gateway
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
     Name = "${var.product}.${var.environment}-gw"
